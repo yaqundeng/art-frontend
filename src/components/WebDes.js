51cbd32ade56e79157edd2532e00206938c7ab55
ethan <han.yic@northeastern.edu>
2022-08-18 10:06:02 -0600

    homepage updated

--- src/components/WebDes.js
+++ src/components/WebDes.js
@@ -1,18 +1,54 @@
-import "./WebDes.css"
+import "./WebDes.css";
+import react, { useCallback, useEffect, useState } from "react";
+import PhotoDataService from "../services/photos";
+import Carousel from 'react-bootstrap/Carousel';
 
-const WebDes = () => {
+const WebDes = ({ }) => {
+
+    const [photos, setPhotos] = useState([]);
+
+    const retrivePhotos = useCallback(() => {
+
+        PhotoDataService.getAll()
+            .then(response => {
+                setPhotos(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }, [photos]);
+
+    useEffect(() => {
+        retrivePhotos();
+    }, [retrivePhotos])
+
     return (
-
-        
         <div>
-            <img src="../homepic.jpg" alt="Home picture" className="homePic"/>
+            <Carousel className="display">
+                {photos.map((photo) => {
+                    return (
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={photo.img}
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>{photo.photo_name}</h3>
+                                <p>{photo.user_name}</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    )
+                })}
+            </Carousel>
             <div className="desContainer">
-                <h1 className="descriptionTitle">We are Hala Madrid</h1>
-                <h5 className="desText">        
-                Maybe you want to keep a blog of moments captured on their cameraphone, 
-                or maybe you want to show off your best pictures to the whole world in a bid for web celebrity. 
-                Or maybe you want to securely and privately share photos of your kids with your family across the country. 
-                We make all these things possible and more!
+            <h3 className="descriptionTitle">Welcome to Artist Portfolio! Click Photos Page to See More.</h3>
+
+                <h5 className="desText">
+                    Maybe you want to keep a blog of moments captured on their cameraphone,
+                    or maybe you want to show off your best pictures to the whole world in a bid for web celebrity.
+                    Or maybe you want to securely and privately share photos of your kids with your family across the country.
+                    We make all these things possible and more!
                 </h5>
             </div>
 
