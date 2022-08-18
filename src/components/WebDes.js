d9d9c68d410035e325f2fb86a8a8ff767de6ab50
wenhuang1 <huangwen@login.khoury.neu.edu>
2022-08-18 11:47:30 -0400

    change layout of landing page

--- src/components/WebDes.js
+++ src/components/WebDes.js
@@ -43,7 +43,8 @@ const WebDes = ({ }) => {
                 })}
             </Carousel>
             <div className="desContainer">
-                {/* <h1 className="descriptionTitle">We are Hala Madrid</h1> */}
+            <h3 className="descriptionTitle">Welcome to Artist Portfolio! Click Photos Page to See More.</h3>
+
                 <h5 className="desText">
                     Maybe you want to keep a blog of moments captured on their cameraphone,
                     or maybe you want to show off your best pictures to the whole world in a bid for web celebrity.
